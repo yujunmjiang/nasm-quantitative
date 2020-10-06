7a00bd65bfc2176f321b78cb46cd430c6981bd58
Yujun Jiang <yujunjiang@gmail.com>
2020-10-06 01:59:45 -0400

    add legends

---
+++ README.md
@@ -0,0 +1,3 @@
+## Project 01 - Air and Space Chronology
+
+<img src="https://github.com/yujunmjiang/major-studio-1-fall-20/blob/master/p1_air_and_space_chronology/document/screencapture-127-0-0-1-5501-visualization-2020-09-21-23_16_23.png">
--- visualization/index.js
+++ visualization/index.js
@@ -109,7 +109,7 @@ grid.selectAll("g")
   // });
   .attr("x", 0)
   .attr("y", function(d, i) {
-    return gap + i * cellSize + 50;
+    return gap + i * cellSize + 0;
   })
   .attr("width", 100)
   .attr("height", 100);
@@ -137,7 +137,7 @@ grid.selectAll("year-cover")
   
   .attr("x", 0)
   .attr("y", function(d, i) {
-    return gap + i * cellSize + 50;
+    return gap + i * cellSize + 0;
   })
   .attr("width", 50)
   .attr("height", 30);
@@ -160,7 +160,7 @@ grid.selectAll("year-cover")
   
   .attr("x", 70)
   .attr("y", function(d, i) {
-    return gap + i * cellSize + 50;
+    return gap + i * cellSize + 0;
   })
   .attr("width", 30)
   .attr("height", 30);
@@ -183,7 +183,7 @@ grid.selectAll("year-cover")
   
   .attr("x", 0)
   .attr("y", function(d, i) {
-    return 70 + gap + i * cellSize + 50;
+    return 70 + gap + i * cellSize + 0;
   })
   .attr("width", 50)
   .attr("height", 30);
@@ -206,7 +206,7 @@ grid.selectAll("year-cover")
   
   .attr("x", 70)
   .attr("y", function(d, i) {
-    return 70 + gap + i * cellSize + 50;
+    return 70 + gap + i * cellSize + 0;
   })
   .attr("width", 30)
   .attr("height", 30);
@@ -234,7 +234,7 @@ grid.selectAll("material")
   
   .attr("x", 0)
   .attr("y", function(d, i) {
-    return 70 + gap + i * cellSize + 50;
+    return 70 + gap + i * cellSize + 0;
   })
   .attr("width", 10)
   .attr("height", 10);
@@ -257,7 +257,7 @@ grid.selectAll("material")
   
   .attr("x", 10)
   .attr("y", function(d, i) {
-    return 70 + gap + i * cellSize + 50;
+    return 70 + gap + i * cellSize + 0;
   })
   .attr("width", 10)
   .attr("height", 10);
@@ -280,7 +280,7 @@ grid.selectAll("material")
   
   .attr("x", 20)
   .attr("y", function(d, i) {
-    return 70 + gap + i * cellSize + 50;
+    return 70 + gap + i * cellSize + 0;
   })
   .attr("width", 10)
   .attr("height", 10);
@@ -303,7 +303,7 @@ grid.selectAll("material")
   
   .attr("x", 30)
   .attr("y", function(d, i) {
-    return 70 + gap + i * cellSize + 50;
+    return 70 + gap + i * cellSize + 0;
   })
   .attr("width", 10)
   .attr("height", 10);
@@ -326,30 +326,62 @@ grid.selectAll("material")
   
   .attr("x", 40)
   .attr("y", function(d, i) {
-    return 70 + gap + i * cellSize + 50;
+    return 70 + gap + i * cellSize + 0;
   })
   .attr("width", 10)
   .attr("height", 10);
 
 
 
+/*************
+Legends
+**************/
+// select the svg area
+var svg = d3.select("#d3-legends")
 
+// legend: color sample
+svg.append("rect").attr("x", 40).attr("y",130).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.8)
+svg.append("rect").attr("x", 40).attr("y",145).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.7)
+svg.append("rect").attr("x", 40).attr("y",160).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.6)
+svg.append("rect").attr("x", 40).attr("y",175).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.5)
+svg.append("rect").attr("x", 40).attr("y",190).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.4)
+svg.append("rect").attr("x", 40).attr("y",205).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.3)
+svg.append("rect").attr("x", 40).attr("y",220).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.2)
+svg.append("rect").attr("x", 40).attr("y",235).attr("width", 15).attr("height", 15).style("fill", "#000000").attr("fill-opacity", 0.1)
 
+svg.append("rect").attr("x", 40).attr("y",330).attr("width", 15).attr("height", 15).style("fill", "#848789")
+svg.append("rect").attr("x", 40).attr("y",350).attr("width", 15).attr("height", 15).style("fill", "#b06650")
+svg.append("rect").attr("x", 40).attr("y",370).attr("width", 15).attr("height", 15).style("fill", "#000000")
+svg.append("rect").attr("x", 40).attr("y",390).attr("width", 15).attr("height", 15).style("fill", "#bfbaaf")
+svg.append("rect").attr("x", 40).attr("y",410).attr("width", 15).attr("height", 15).style("fill", "#ffbdee")
+svg.append("rect").attr("x", 40).attr("y",430).attr("width", 15).attr("height", 15).style("fill", "#eeeeee")
+svg.append("rect").attr("x", 40).attr("y",450).attr("width", 15).attr("height", 15).style("fill", "#d65720")
+svg.append("rect").attr("x", 40).attr("y",470).attr("width", 15).attr("height", 15).style("fill", "#cabfbb")
+svg.append("rect").attr("x", 40).attr("y",490).attr("width", 15).attr("height", 15).style("fill", "#43464b")
+svg.append("rect").attr("x", 40).attr("y",510).attr("width", 15).attr("height", 15).style("fill", "#855e42")
 
-// var text = grid.selectAll(null)
-//   .data(dataset)
-//   .enter()
-//   .append("text");
+// legend: text
+svg.append("text").attr("x", 40).attr("y", 100).text("FIRST SERVICE YEAR").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
 
-// var textTitles = text
-//   .attr("x", function(d, i) {
-//     return gap + i * cellSize + 50;
-//   })
-//   .attr("y", function(d, i) {
-//     return gap + i * cellSize + 50;
-//   })
-//   .text(function(d) {return d.title})
-//   .attr("font-family", "sans-serif")
-//   .attr("font-size", "10px")
-//   .attr("fill", "red");
+svg.append("text").attr("x", 60).attr("y", 130).text("Later").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 145).text("2010").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 160).text("1990").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 175).text("1970").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 190).text("1950").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 205).text("1930").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 220).text("1910").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 235).text("1890").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 250).text("Earlier").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
 
+svg.append("text").attr("x", 40).attr("y", 300).text("MATERIALS").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+
+svg.append("text").attr("x", 60).attr("y", 338).text("Aluminum").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 358).text("Copper").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 378).text("Cord").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 398).text("Cotton").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 418).text("Fabric").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 438).text("Plastic").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 458).text("Rubber").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 478).text("Silk").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 498).text("Steel").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
+svg.append("text").attr("x", 60).attr("y", 518).text("Wood").style("font-size", "10px").attr("alignment-baseline","middle").style("fill", "#ffffff")
